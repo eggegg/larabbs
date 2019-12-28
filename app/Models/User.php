483a7cfca9ee410f011851b6b29f718e4e982e28
Egg Egg <eggegg01@126.com>
2019-12-28 20:30:34 +0800

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTraits;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use Notifiable, MustVerifyEmailTraits;
 
     /**
      * The attributes that are mass assignable.
