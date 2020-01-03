54f5f1ddccd3142c083be6b665fc161e2ec12e96
eggegg <eggegg01@126.com>
2020-01-03 10:20:54 +0000

    回复数据

--- public/js/app.js
+++ public/js/app.js
@@ -49485,8 +49485,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/l5b.test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/l5b.test/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/l5a.test/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/l5a.test/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
